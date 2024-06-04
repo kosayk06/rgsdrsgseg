46eeb492014896417cd6c2ccd7c9d3cdde838043
kosayk06 <122724516+kosayk06@users.noreply.github.com>
2024-06-04 17:20:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let a = document.querySelector(".model");
+let b = document.querySelector(".open-model");
+let btn = document.querySelector(".btn");
+let tt = document.querySelector(".w");
+
+b.addEventListener("click", function() {
+      a.classList.add("open");
+});
+
+btn.addEventListener("click", function(){
+    a.classList.remove("open")
+}
+)
+tt.addEventListener("click", function(){
+    a.classList.remove("open")
+}
+)
+
+
+
+
+
+
+
